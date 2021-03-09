a65848cd1c19b06b5069eaedb5c44af5e82724a8
先規　勇樹 <yukisaki80@gmail.com>
2021-03-09 16:27:35 +0900

    initial commit

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+
+class PostsController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+
+        return view('posts.index', ['posts' => $posts]);
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        $params = $request->validate([
+            'title' => 'required|max:50',
+            'body' => 'required|max:2000',
+        ]);
+
+        Post::create($params);
+
+        return redirect()->route('top');
+    }
+
+    public function show($post_id)
+    {
+        $post = Post::findOrFail($post_id);
+
+        return view('posts.show', [
+            'post' => $post,
+        ]);
+    }
+}
